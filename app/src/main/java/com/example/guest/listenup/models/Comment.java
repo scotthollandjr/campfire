9dcafc2379c134796b704b4b4c31721f652f17c5
sm <scottmcintire90@gmail.com>
2016-07-14 12:27:17 -0700

    fix problems with firebase chat

--- app/src/main/java/com/example/guest/listenup/models/Comment.java
+++ app/src/main/java/com/example/guest/listenup/models/Comment.java
@@ -4,16 +4,22 @@ package com.example.guest.listenup.models;
  * Created by Guest on 7/14/16.
  */
 public class Comment {
-    private String mContent;
+    private String Content;
+    private String User;
 
     public Comment() {}
 
-    public Comment(String content) {
-        this.mContent = content;
+    public Comment(String content, String user) {
+        this.Content = content;
+        this.User = user;
     }
 
     public String getContent() {
-        return mContent;
+        return Content;
+    }
+
+    public String getUser() {
+        return User;
     }
 }
 
--- app/src/main/java/com/example/guest/listenup/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/listenup/ui/MainActivity.java
@@ -4,42 +4,76 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.guest.listenup.R;
+import com.example.guest.listenup.models.Comment;
+import com.firebase.client.Firebase;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.commentEditText) EditText mCommentEditText;
-    @Bind(R.id.commentListView) ListView mCommentListView;
-
+    @Bind(R.id.sendButton) Button mSend;
+    private Firebase mFirebaseRef;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userName;
+
+//    private ChatListAdapter mChatListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Firebase.setAndroidContext(this);
+        mFirebaseRef = new Firebase("https://listenup-51c14.firebaseio.com/");
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
+                userName = user.getDisplayName();
                 if (user != null) {
                     getSupportActionBar().setTitle("Welcome, " + user.getDisplayName() + "!");
                 } else {
-
                 }
             }
         };
+
+        mSend.setOnClickListener(this);
+}
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSend) {
+            String message = mCommentEditText.getText().toString();
+            Map<String, Object> values = new HashMap<>();
+            values.put("name", userName);
+            values.put("message", message);
+            mFirebaseRef.push().setValue(values);
+            mCommentEditText.setText("");
+        }
     }
 
     @Override
